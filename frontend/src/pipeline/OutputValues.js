47c63d33d2d61c03e2702748398494545b2f8b8f
Ewout Prangsma <ewout@prangsma.net>
2018-11-20 13:27:26 +0100

    Creation timestamp

--- frontend/src/pipeline/OutputValues.js
+++ frontend/src/pipeline/OutputValues.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Header, Table } from 'semantic-ui-react';
+import moment from 'moment';
 
-const RowView = ({id,output,data}) => (
+const RowView = ({id,output,data,created_at}) => (
   <Table.Row>
     <Table.Cell>{id}</Table.Cell>
     <Table.Cell>{output}</Table.Cell>
     <Table.Cell>{data}</Table.Cell>
+    <Table.Cell>{moment(created_at).fromNow()}</Table.Cell>
   </Table.Row>
 );
 
@@ -16,6 +18,7 @@ const TableView = ({values}) => (
         <Table.HeaderCell>ID</Table.HeaderCell>
         <Table.HeaderCell>Output</Table.HeaderCell>
         <Table.HeaderCell>Data</Table.HeaderCell>
+        <Table.HeaderCell>When</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
     <Table.Body>
@@ -24,6 +27,7 @@ const TableView = ({values}) => (
         id={value.id}
         output={value.source_task_output}
         data={value.data}
+        created_at={value.created_at}
       />)}
     </Table.Body>
   </Table>
